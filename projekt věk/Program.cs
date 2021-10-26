a22a82bf5d7b33759d66ffb8535befc7da1e1527
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2021-10-26 11:35:25 +0200

    vek

--- projekt věk/Program.cs
+++ projekt věk/Program.cs
@@ -6,7 +6,50 @@ namespace projekt_věk
     {
         static void Main(string[] args)
         {
+            double rok;
+            double vek;
+
+             
             Console.WriteLine("Zadej rok narození. ");
+            Double.TryParse(Console.ReadLine(), out rok);
+            vek = DateTime.Now.Year - rok;
+            if (vek < 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ještě si se nenarodil.");
+
+            }
+            else if (vek < 15)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Jsi ještě dítě :D");
+            }
+            else if (vek < 19)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Pravděpodobně jsi student");
+            }
+            else if (vek < 26)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Nejlepší léta");
+            }
+            else if (vek < 50)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Užívej dokud můžeš");
+            }
+            else if (vek < 80)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Jste důchodce");
+            }
+            else if (vek < 100)
+            { 
+                    
+
         }
+
     }
+
 }
