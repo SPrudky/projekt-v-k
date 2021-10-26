de4827d60da83cfc743db965012a4a2ae9d896bc
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2021-10-26 10:27:14 +0200

    Add project files.

---
+++ projekt věk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projekt_věk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej rok narození. ");
+        }
+    }
+}
